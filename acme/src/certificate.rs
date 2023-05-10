a38e3b53f639ecdbc90fdd16d6e7504a2d43fbf9
beltram <beltram.maldant@gmail.com>
2023-05-10 15:13:32 +0200

    feat!: test templating the generated x509 certificate and add verifications in the certificate parsing step that the generated certificate contains the expected identity claims

--- acme/src/certificate.rs
+++ acme/src/certificate.rs
@@ -1,5 +1,7 @@
-use crate::prelude::*;
 use rusty_jwt_tools::prelude::*;
+use x509_cert::Certificate;
+
+use crate::prelude::*;
 
 impl RustyAcme {
     /// For fetching the generated certificate
@@ -21,19 +23,42 @@ impl RustyAcme {
     }
 
     /// see [RFC 8555 Section 7.4.2](https://www.rfc-editor.org/rfc/rfc8555.html#section-7.4.2)
-    pub fn certificate_response(response: String) -> RustyAcmeResult<Vec<Vec<u8>>> {
+    pub fn certificate_response(response: String, order: AcmeOrder) -> RustyAcmeResult<Vec<Vec<u8>>> {
         let pems: Vec<pem::Pem> = pem::parse_many(response)?;
         pems.into_iter()
             .enumerate()
-            .try_fold(vec![], |mut acc, (i, cert_pem)| -> RustyAcmeResult<Vec<Vec<u8>>> {
+            .try_fold(vec![], move |mut acc, (i, cert_pem)| -> RustyAcmeResult<Vec<Vec<u8>>> {
                 use x509_cert::der::Decode as _;
                 let cert = x509_cert::Certificate::from_der(cert_pem.contents())?;
                 // only verify that leaf has the right identity fields
                 if i == 0 {
-                    let _ = cert.extract_identity()?;
+                    Self::verify_leaf_certificate(order.clone(), cert)?;
                 }
                 acc.push(cert_pem.contents().to_vec());
                 Ok(acc)
             })
+    }
+
+    /// Ensure that the generated certificate matches our expectations (i.e. that the acme server is configured the right way)
+    /// We verify that the fields in the certificate match the ones in the ACME order
+    fn verify_leaf_certificate(mut order: AcmeOrder, cert: Certificate) -> RustyAcmeResult<()> {
+        let cert_identity = cert.extract_identity()?;
+
+        let identifier = order.identifiers.pop().ok_or(RustyAcmeError::ImplementationError)?;
+        let identifier = identifier.to_wire_identifier()?;
+
+        let invalid_client_id =
+            ClientId::try_from_qualified(&cert_identity.client_id)? != ClientId::try_from_uri(&identifier.client_id)?;
+
+        let invalid_display_name = cert_identity.display_name != identifier.display_name;
+
+        let invalid_handle = cert_identity.handle != identifier.handle.trim_start_matches(ClientId::URI_PREFIX);
+
+        let invalid_domain = cert_identity.domain != identifier.domain;
+
+        if invalid_display_name || invalid_client_id || invalid_handle || invalid_domain {
+            return Err(RustyAcmeError::InvalidCertificate);
+        }
+        Ok(())
     }
 }
