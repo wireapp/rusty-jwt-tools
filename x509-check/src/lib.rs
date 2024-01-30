a6da7d2857f7d0eaad6519296272f8a4bb368eb2
beltram <beltram.maldant@gmail.com>
2024-01-30 12:40:58 +0100

    fix: we were trying to decode ski & aki as utf-8

--- x509-check/src/lib.rs
+++ x509-check/src/lib.rs
@@ -21,6 +21,12 @@ pub enum RustyX509CheckError {
     /// Error when we have no idea what the cert status is
     #[error("Something went wrong, we cannot determine if this certificate is OK. You might want to ignore this")]
     CannotDetermineVerificationStatus,
+    /// Required 'Subject Key Identifier' extension is missing
+    #[error("Required 'Subject Key Identifier' extension is missing")]
+    MissingSki,
+    /// Implementation error
+    #[error("Implementation error")]
+    ImplementationError,
 }
 
 impl From<certval::Error> for RustyX509CheckError {
