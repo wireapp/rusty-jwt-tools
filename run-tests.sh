f74cc3fadf93629cba05a72cdba3db63433d9122
Ivan Stanković <ivan.stankovic@wire.com>
2025-07-29 14:44:02 +0200

    test: add run-tests.sh script
    The script makes sure the test environment is set up properly.

---
+++ run-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This script is used to start test-wire-server before the test suite is run,
+# because starting Keycloak requires that the redirect URL, which points to the
+# test Wire server, is known.
+
+tmpfile=$(mktemp)
+rm ${tmpfile}
+mkfifo ${tmpfile}
+cargo run --locked test-wire-server > ${tmpfile} &
+test_wire_server_pid=$!
+
+# The test suite needs this environment variable in order to set up the test
+# environment.
+read TEST_WIRE_SERVER_ADDR < ${tmpfile}
+export TEST_WIRE_SERVER_ADDR
+
+cargo nextest run --locked
+
+# Clean up.
+docker kill keycloak && docker rm keycloak
+kill ${test_wire_server_pid}
+rm ${tmpfile}
